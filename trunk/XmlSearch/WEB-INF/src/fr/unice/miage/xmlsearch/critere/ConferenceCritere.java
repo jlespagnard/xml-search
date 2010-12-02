7b25b3059173a03952c8511768efab4cc4249386
julien.lespagnard@gmail.com <julien.lespagnard@gmail.com@6a03aaa1-9c10-afba-a635-7bf7995b2f5c>
2010-12-02 17:59:53 +0000

    Conférence :   - Ajout du container des conférences.   - Ajout du critère pour les conférences.   - Ajout du DAO pour les conférences.
    Utils :
      - Ajout d'une méthode générique permettant de lire un document XML et de renvoyer ses contenus textes triés par nom et redondance.

---
+++ trunk/XmlSearch/WEB-INF/src/fr/unice/miage/xmlsearch/critere/ConferenceCritere.java
@@ -0,0 +1,57 @@
+package fr.unice.miage.xmlsearch.critere;
+
+import java.util.List;
+import fr.unice.miage.xmlsearch.critere.Critere;
+
+/**
+ * @author Julien Lespagnard
+ * @author Elodie Mazuel
+ * @version 1.0
+ */
+public class ConferenceCritere extends Critere {
+	public static final String TITRES = "titres";
+	public static final String LIEUX = "lieux";
+	public static final String PAYS = "pays";
+	public static final String ANNEES = "annees";
+	
+	/** Les titres des conf&eacute;rences. */
+	private List<String> m_titres;
+	/** Les lieux des conf&eacute;rences. */
+	private List<String> m_lieux;
+	/** Les pays dans lesquels ont eu lieu les conf&eacute;rences. */
+	private List<String> m_pays;
+	/** Les ann&eacute;s des conf&eacute;rences. */
+	private List<String> m_annees;
+	
+	/**
+	 * 
+	 * @param p_titres	les titres des conf&eacute;rences
+	 * @param p_lieux	les lieux des conf&eacute;rences
+	 * @param p_pays	les pays dans lesquels ont eu lieu les conf&eacute;rences
+	 * @param p_annees	les ann&eacute;s des conf&eacute;rences
+	 */
+	public ConferenceCritere(List<String> p_titres, List<String> p_lieux, 
+			List<String> p_pays, List<String> p_annees) {
+		this.m_titres = p_titres;
+		this.m_lieux = p_lieux;
+		this.m_pays = p_pays;
+		this.m_annees = p_annees;
+	}
+	
+	@Override
+	public List<String> get(String p_nomCritere) {
+		if(p_nomCritere == null || p_nomCritere.isEmpty())
+			return null;
+		
+		if(p_nomCritere.equalsIgnoreCase(ConferenceCritere.TITRES))
+			return this.m_titres;
+		if(p_nomCritere.equalsIgnoreCase(ConferenceCritere.LIEUX))
+			return this.m_lieux;
+		if(p_nomCritere.equalsIgnoreCase(ConferenceCritere.PAYS))
+			return this.m_pays;
+		if(p_nomCritere.equalsIgnoreCase(ConferenceCritere.ANNEES))
+			return this.m_annees;
+		
+		return null;
+	}
+}
