9d7ce651b4481368eb739aca61da7e8a5a8f9721
julien.lespagnard@gmail.com <julien.lespagnard@gmail.com@6a03aaa1-9c10-afba-a635-7bf7995b2f5c>
2010-12-11 18:15:09 +0000

    Correction de bugs dans la recherche de projet et dans la récupération du nombre de conférences par pays.

--- XmlSearch/WEB-INF/src/fr/unice/miage/xmlsearch/servlets/GetProjet.java
+++ XmlSearch/WEB-INF/src/fr/unice/miage/xmlsearch/servlets/GetProjet.java
@@ -10,14 +10,15 @@ import org.json.JSONObject;
 
 import fr.unice.miage.xmlsearch.dao.ProjetDAO;
 import fr.unice.miage.xmlsearch.objets.Projet;
+import fr.unice.miage.xmlsearch.utils.Constantes;
 
 @SuppressWarnings("serial")
 public class GetProjet extends Servlet {
 	@Override
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		String annee = req.getParameter("annee");
-		String shortName = req.getParameter("shortName");
+		String annee = req.getParameter(Constantes.Projet.ANNEE.getLabel());
+		String shortName = req.getParameter(Constantes.Projet.SHORT_NAME.getLabel());
 		
 		ProjetDAO daoProjet = new ProjetDAO(this.getServletContext().getInitParameter(NOM_PARAMETRE_CONTEXTE));
 		Projet projet = daoProjet.getProjet(shortName, annee);
