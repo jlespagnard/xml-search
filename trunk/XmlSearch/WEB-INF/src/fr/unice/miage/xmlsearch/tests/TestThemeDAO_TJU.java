64e353a3fd2bb10608a97a5168f1937eaa76bde8
mazuel.elodie <mazuel.elodie@6a03aaa1-9c10-afba-a635-7bf7995b2f5c>
2010-12-12 18:05:00 +0000

    modification theme par annee

--- trunk/XmlSearch/WEB-INF/src/fr/unice/miage/xmlsearch/tests/TestThemeDAO_TJU.java
+++ trunk/XmlSearch/WEB-INF/src/fr/unice/miage/xmlsearch/tests/TestThemeDAO_TJU.java
@@ -3,6 +3,7 @@ package fr.unice.miage.xmlsearch.tests;
 import static org.junit.Assert.*;
 
 import java.util.List;
+import java.util.Map;
 
 import org.junit.Test;
 
@@ -14,11 +15,12 @@ public class TestThemeDAO_TJU {
 	@Test
 	public void testGetThemeParAnnee() {
 		ThemeDAO dao = new ThemeDAO("http://localhost:8086/exist/rest/db/Raweb/query/");
-		List<Theme> liste = dao.getThemeParAnnee("2006");
+		Map<String,String> liste = dao.getThemeParAnnee("2009");
 		assertNotNull(liste);
 		assertFalse(liste.isEmpty());
-		assertTrue(liste.size() == 5);	
-		assertTrue(liste.get(0).getLibelle().compareTo("SYM")==0);
+		assertTrue(liste.size() == 20);	
+		assertTrue(liste.get("Computational Biology and Bioinformatics").compareTo("6")==0);
+		assertTrue(liste.get("Networks and Telecommunications").compareTo("13")==0);
 	}
 
 }
